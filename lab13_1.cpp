b89186962bdbbe37e7349e6e6335be9dad678307
min23asdw <min23asdw@outlook.com>
2021-01-22 18:10:50 +0700

    D

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -3,6 +3,19 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+  for(int i=0;i<N-1;i++){
+	 cout<< "Pass "<< i+1 << ":";
+	  for(int j=i;j>=0;j--){
+     if(d[j+1]>=d[j]){
+       T temp = d[j+1];
+       d[j+1]=d[j];
+       d[j]=temp;
+
+	   }
+   }
+     for(int b=0;b<N;b++){cout << d[b] << " ";}
+  cout<< "\n";
+  }
 
 }
 
@@ -11,9 +24,9 @@ int main(){
 	cout << "Input Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";
 	cout << "\n\n";
-	
+
 	insertionSort(a,10);
-	
+
 	cout << "\nSorted Array:";
-	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
+	for(int i = 0; i < 10; i++) cout << a[i] << " ";
 }
--- lab13_2.cpp
+++ lab13_2.cpp
@@ -10,12 +10,64 @@ void findLocalMax(const double [][N], bool [][N]);
 void showMatrix(const bool [][N]);
 
 int main(){
-	double A[N][N]; 
-	bool B[N][N]; 
+	double A[N][N];
+	bool B[N][N];
 	cout << "Input the matrix.....\n";
 	inputMatrix(A);
 	findLocalMax(A,B);
 	cout << "Result is \n";
 	showMatrix(B);
 	return 0;
+}
+void inputMatrix(double A[N][N]){
+	for(int i = 0;i<N;i++){
+		cout << "Row " << i+1 <<": ";
+		for(int j=0;j<N;j++){
+			cin >>A[i][j];
+
+		}
+
+	}
+}
+
+
+
+void findLocalMax(const double A[N][N], bool B[N][N]){
+
+	for(int j=0;j<N;j++){
+		for(int i=0;i<N;i++){
+
+			B[j][i]= 0;
+			if(A[j][i]>=A[j+1][i] && A[j][i]>=A[j-1][i]&& A[j][i]>=A[j][i+1] && A[j][i]>=A[j][i-1])
+			{
+
+				B[j][i]= 1;
+			}
+
+			B[j][0]=0;
+			B[j][N-1]=0;
+			B[0][i]=0;
+			B[N-1][i]=0;
+
+		}
+	}
+
+
+
+}
+
+
+
+
+void showMatrix(const bool A[N][N]){
+	for(int j=0;j<N;j++){
+		for(int i=0;i<N;i++){
+			cout << A[j][i] << " ";
+		}
+		cout << "\n";
+	}
+
+
+
+
 }
